89b9bfb04da59d8362b63f0ffbb66c476fb282d6
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-05-19 19:33:54 +0900

    宣言を分離(複数翻訳単位からインクルードのため

--- SignalParser.cxx
+++ SignalParser.cxx
@@ -8,6 +8,8 @@
 
 #include <cstdint>
 
+#include "SignalParser.h"
+
 #if 0
 struct signal_prop {
 	uint32_t id;
@@ -33,21 +35,9 @@ std::vector<struct signal_prop> & SignalParser::Parsing(std::string &sigstr)
 }
 #endif
 
-struct psig{
-	uint32_t group_id;
-	uint32_t subgroup_id;
-	uint32_t femId;
-	uint32_t channel;
-	uint32_t offset;
-};
+
 
 namespace SignalParser {
-
-constexpr uint32_t NO_SUBGROUP = UINT32_MAX;
-
-const std::string bra("(");
-const std::string ket(")");
-const std::string delim(" ");
 
 std::vector<std::string> bksplit(const std::string str)
 {
