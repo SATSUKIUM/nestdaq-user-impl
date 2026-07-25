b4c36508cc05011b3f832ad82f1835b6611ce268
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-07-26 01:53:34 +0900

    detconfをどこから読み出すかcout

--- FilterTimeFrameSliceByTrack.cxx
+++ FilterTimeFrameSliceByTrack.cxx
@@ -354,7 +354,8 @@ bool FilterTimeFrameSliceByTrack::RegisterDetectorConfig_Geometry()
       std::string PlaneName = detectorPlaneMap[detectorId];
       int SegmentNumber = detectorSegmentMap[detectorId];
       std::string ChannelName;
-      fChMap->detname_dictionary.invIndex(0, ChannelName);
+      uint8_t ChannelNameIdx = fChMap->detname_dictionary.queryIndex_readout_channel("0");
+      fChMap->detname_dictionary.invIndex(ChannelNameIdx, ChannelName);
       #if CHECK_COUT_DETCONF_REGISTERING
       std::cout << "\tDetectorName: " << DetectorName << std::endl;
       std::cout << "\tPlaneName: " << PlaneName << std::endl;
