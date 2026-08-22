f8df2aa901cf2f5baebb5bba93324e2989d4562f
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-08-22 11:07:06 +0900

    型記載ミス

--- FilterTimeFrameSliceByTrack.cxx
+++ FilterTimeFrameSliceByTrack.cxx
@@ -488,7 +488,7 @@ bool FilterTimeFrameSliceByTrack::RegisterDetectorConfig_DCTdcCalib()
                   #if 1
                   fChMap->getDETIdItem(dopeKeyFEtoDET).decode();
                   #endif
-                  fChMap->registerDETConfSubItem<chmap::GeomItem, chmap::CalibrationItem_DCTdcCalib>(dopeKeyFEtoDET, std::move(calibitem_dctdccalib), &chmap::DETConfItem::geom);
+                  fChMap->registerDETConfSubItem<chmap::CalibrationItem, chmap::CalibrationItem_DCTdcCalib>(dopeKeyFEtoDET, std::move(calibitem_dctdccalib), &chmap::DETConfItem::geom);
                } // if(found_FEtoDET)
                else{
                   std::cout << funcname << "no DETIdItem found" << std::endl;
@@ -569,7 +569,7 @@ bool FilterTimeFrameSliceByTrack::RegisterDetectorConfig_DCDriftParam()
                   #if 1
                   fChMap->getDETIdItem(dopeKeyFEtoDET).decode();
                   #endif
-                  fChMap->registerDETConfSubItem<chmap::GeomItem, chmap::CalibrationItem_DCDriftLength>(dopeKeyFEtoDET, std::move(calibitem_dcdriftlength), &chmap::DETConfItem::geom);
+                  fChMap->registerDETConfSubItem<chmap::CalibrationItem, chmap::CalibrationItem_DCDriftLength>(dopeKeyFEtoDET, std::move(calibitem_dcdriftlength), &chmap::DETConfItem::geom);
                } // if(found_FEtoDET)
                else{
                   std::cout << funcname << "no DETIdItem found" << std::endl;
