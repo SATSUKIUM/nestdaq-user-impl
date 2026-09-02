250dde22e2e90b3cbd69e92c5ca8ac105e52fed2
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-09-02 17:49:06 +0900

    logicfilterの動作確認

--- SignalParser.h
+++ SignalParser.h
@@ -1,29 +1,29 @@
-#ifndef SIGNALPARSER_H
-#define SIGNALPARSER_H
+// #ifndef SIGNALPARSER_H
+// #define SIGNALPARSER_H
 
-#include <iostream>
-#include <vector>
-#include <string>
+// #include <iostream>
+// #include <vector>
+// #include <string>
 
-#include <cstdint>
+// #include <cstdint>
 
-struct psig{
-	uint32_t group_id;
-	uint32_t subgroup_id;
-	uint32_t femId;
-	uint32_t channel;
-	uint32_t offset;
-};
+// struct psig{
+// 	uint32_t group_id;
+// 	uint32_t subgroup_id;
+// 	uint32_t femId;
+// 	uint32_t channel;
+// 	uint32_t offset;
+// };
 
-namespace SignalParser{
+// namespace SignalParser{
 
-constexpr uint32_t NO_SUBGROUP = UINT32_MAX;
-const std::string bra("(");
-const std::string ket(")");
-const std::string delim(" ");
+// constexpr uint32_t NO_SUBGROUP = UINT32_MAX;
+// const std::string bra("(");
+// const std::string ket(")");
+// const std::string delim(" ");
 
-std::vector<std::string> bksplit(const std::string str);
-std::vector<struct psig> Parsing(const std::string &sigstr);
-} // namespace SignalParser
+// std::vector<std::string> bksplit(const std::string str);
+// std::vector<struct psig> Parsing(const std::string &sigstr);
+// } // namespace SignalParser
 
-#endif
+// #endif
