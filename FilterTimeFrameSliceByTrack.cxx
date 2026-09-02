7fa5ca77f68dc65a7ab7d69da65439796ceb18e8
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-09-02 16:20:03 +0900

    なんかおかしい。デバッグ

--- FilterTimeFrameSliceByTrack.cxx
+++ FilterTimeFrameSliceByTrack.cxx
@@ -278,7 +278,8 @@ bool FilterTimeFrameSliceByTrack::ProcessSlice(TTF& tf)
                ftdc = tdc64_h.tdc * 0.9765625 * 0.001; // HR TDCのLSBが0.9765625 ps = 1/2^10 nsなので、(0.9765625 * 0.001)を掛ける
                ftdc_int = tdc64_h.tdc>>10; // HR TDCのLSBが0.9765625 ps = 1/2^10 nsなので、(0.9765625 * 0.001)を掛ける代わりに2^10を掛ける
                #if CHECK_COUT_UTOF_TIMING
-               std::cout << funcname << "utof right: ch = " << ch << ", tdc - lftdc = " << ftdc - lftdc << std::endl;
+               std::cout << funcname << "(double) utof right: ch = " << ch << ", tdc     - lftdc = " << ftdc - lftdc << std::endl;
+               std::cout << funcname << "(int)    utof right: ch = " << ch << ", tdc_int - lftdc = " << static_cast<int>(ftdc_int) - static_cast<int>(lftdc) << std::endl;
                #endif
                if(ftdc >= tdc_min && ftdc <= tdc_max){
                   nTDC_utof_right++;
@@ -290,7 +291,7 @@ bool FilterTimeFrameSliceByTrack::ProcessSlice(TTF& tf)
                ftdc_int = tdc64_h.tdc>>10; // HR TDCのLSBが0.9765625 ps = 1/2^10 nsなので、(0.9765625 * 0.001)を掛ける代わりに2^10を掛ける
                #if CHECK_COUT_UTOF_TIMING
                std::cout << funcname << "(double) utof left: ch = " << ch << ", tdc     - lftdc = " << ftdc - lftdc << std::endl;
-               std::cout << funcname << "(int)    utof left: ch = " << ch << ", tdc_int - lftdc = " << ftdc_int - lftdc << std::endl;
+               std::cout << funcname << "(int)    utof left: ch = " << ch << ", tdc_int - lftdc = " << static_cast<int>(ftdc_int) - static_cast<int>(lftdc) << std::endl;
                #endif
                if(ftdc >= tdc_min && ftdc <= tdc_max){
                   nTDC_utof_left++;
