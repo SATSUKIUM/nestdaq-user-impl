c38bdea5b37f45de47d9df89a41f792e72b11079
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-09-03 15:42:17 +0900

    kldcのヒットを集める処理を書いた

--- FilterTimeFrameSliceByTrack.h
+++ FilterTimeFrameSliceByTrack.h
@@ -42,7 +42,6 @@ struct nestdaq::DCRawHit {
 
 class nestdaq::DCHit {
 public:
-   DCHit();
    DCHit(double wirePos, double wireAngle, const chmap::DETIdItem* detid) {
       this->wirePos = wirePos;
       this->wireAngle = wireAngle;
