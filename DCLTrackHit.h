40f9e45f057205775015fdc070a4bb24d34b53ff
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-09-06 17:05:19 +0900

    コンパイルエラー対処

--- DCLTrackHit.h
+++ DCLTrackHit.h
@@ -4,6 +4,7 @@
 #include "DCHit.h"
 
 namespace nestdaq{
+    class DCHit;
     class DCLTrackHit{
     public:
         DCLTrackHit(const DCHit* parent_, int nth_, double w_, int leftright) : parent(parent_), nth(nth_), w(w_), leftright(leftright) {
