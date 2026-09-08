cbf84d637b98ac5dd1eb0ad316e777c9864647b9
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-09-09 00:51:41 +0900

    計測

--- FilterTimeFrameSliceByTrack.h
+++ FilterTimeFrameSliceByTrack.h
@@ -193,7 +193,7 @@ protected:
       {4,5}, // KLDC2 UU'
       {6,7}  // KLDC2 VV'
    };
-   static constexpr int fNumAngleCorrectionIteration = 1;
+   static constexpr int fNumAngleCorrectionIteration = 0;
 
    bool MakePairPlaneHitCluster(std::vector<DCHit>& HC1, std::vector<DCHit>& HC2, double cellSize, std::vector<DCPairHitCluster*>& Cont);
 
